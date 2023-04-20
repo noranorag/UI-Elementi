54b399ea60e97a5d62393936b82f967806bd6ad1
Nora <nora.galina@sk.lvt.lv>
2023-04-20 12:16:00 +0300

    Uztaisits vis kaut kas

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour {
+    public GameObject binsImg;
+    public GameObject lacitisImg;
+    public GameObject omeImg;
+    public GameObject masinaImg;
+    public GameObject paKreisiPoga;
+    public GameObject paLabiPoga;
+    public GameObject mainigaisAttels;
+    public Sprite[] atteluMasivs;
+
+    public void binaAttelosana(bool vertiba)
+    {
+        binsImg.SetActive(vertiba);
+        paKreisiPoga.GetComponent<Toggle>().interactable = vertiba;
+        paLabiPoga.GetComponent<Toggle>().interactable = vertiba;
+
+
+    }
+
+    public void lacisaAttelosana(bool vertiba)
+    {
+        lacitisImg.SetActive(vertiba);
+    }
+
+    public void omesAttelosana(bool vertiba)
+    {
+        omeImg.SetActive(vertiba);
+    }
+
+    public void masinasAttelosana(bool vertiba)
+    {
+        masinaImg.SetActive(vertiba);
+    }
+
+    public void paKreisiBins()
+    {
+        binsImg.transform.localScale = new Vector2(1, 1);
+
+    }
+
+    public void paLabiBins()
+    {
+        binsImg.transform.localScale = new Vector2(-1, 1);
+
+    }
+
+    public void izkritosais(int skaitlis)
+    {
+        if(skaitlis == 0)
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+
+        else if (skaitlis == 1)
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+
+        else if (skaitlis == 2)
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[2];
+        Debug.Log("Nav piesaistīts attēls!");
+    }
+
+
+
+
+}
--- Assets/Skripti/VardaParadisana.cs
+++ Assets/Skripti/VardaParadisana.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VardaParadisana : MonoBehaviour {
-	
+	private string teksts;
+	public GameObject ievadesLauks;
+	public GameObject tekstaAttelosana;
+	private string[] fragmenti = {"Sveiks ", "Jauku dienu ", "Prieks tevi redzēt ", "Uzredzēšanos " };
+	int indekss;
+	public void UzglabatTekstu()
+	{
+		indekss = Random.Range(0, fragmenti.Length);
+		teksts = ievadesLauks.GetComponent<InputField>().text;
+		tekstaAttelosana.GetComponent<Text>().text = fragmenti[indekss] + teksts.ToUpper() + "!";
+	}
 
-	// Use this for initialization
-	void Start () {
-		
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-		
-	}
 }
