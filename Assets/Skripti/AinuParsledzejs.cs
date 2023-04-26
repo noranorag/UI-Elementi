ba502f37b42a6843a067e7d06f0517fae324724e
Nora <nora.galina@sk.lvt.lv>
2023-04-26 12:10:06 +0300

    Izveidots sakuma ekrans

---
+++ Assets/Skripti/AinuParsledzejs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AinuParsledzejs : MonoBehaviour {
+
+	//metode lauj parslegties no UI ainas un sakuma ainu
+	public void uzSakumu()
+	{
+		SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+	}
+
+	//Pasiem jauztaisa metode, kas parsledz no sakuma uz UI ainu
+
+	public void uzUI()
+	{
+		SceneManager.LoadScene("UI elementi", LoadSceneMode.Single);
+	}
+
+	//metode kura aizver programmu ja ta ir izveidota ka .exe
+
+	public void Apturet()
+	{
+		Application.Quit();
+	}
+
+
+
+}
---
+++ Assets/Skripti/KursorsUzAttela.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KursorsUzAttela : MonoBehaviour {
+
+	public AudioSource skanasAvots;
+	public AudioClip skanaKoAtskanot;
+
+	public void uzbiditUzAttela()
+	{
+		skanasAvots.PlayOneShot(skanaKoAtskanot);
+	}
+
+	public void kursorsNost()
+	{
+		skanasAvots.Stop();
+	}
+	
+}
